2cc4bc4d5da58cffe1fd53c9d667ca749025f7c5
moriyam-mohona <moriyammohona@gmail.com>
2024-03-27 15:12:06 +0600

    BarChart Section

--- src/Components/PagesToRead/PagesToRead.jsx
+++ src/Components/PagesToRead/PagesToRead.jsx
@@ -1,11 +1,55 @@
+import { useEffect, useState } from 'react';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, Cell } from 'recharts';
+import { getBooks } from '../../utils'; // Assuming you have a utility function to get books from local storage
 
+const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
-const PagesToRead = () => {
+const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
+  Z`;
+};
+
+const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
+
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+
+const BookChart = () => {
+    const [bookData, setBookData] = useState([]);
+
+    useEffect(() => {
+
+        const storedBooks = getBooks();
+
+        const formattedData = storedBooks.map((book, index) => ({
+            name: book.bookName,
+            uv: book.totalPages,
+            fill: colors[index % colors.length],
+        }));
+
+        setBookData(formattedData);
+    }, []);
+
     return (
-        <div>
-            <h3>Pages To Read</h3>
+        <div className='bg-gray-100 px-10 py-20'>
+            <BarChart width={1000} height={400} data={bookData}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" angle={-90} interval={0} textAnchor="end" />
+                <YAxis />
+                <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#ccc' }} />
+
+                <Bar dataKey="uv" fill="#8884d8" shape={<TriangleBar></TriangleBar>} >
+                    {bookData.map((entry, index) => (
+
+                        <Cell key={`cell-${index}`} fill={entry.fill} />
+                    ))}
+                </Bar>
+            </BarChart>
         </div>
     );
 };
 
-export default PagesToRead;
+export default BookChart;
--- src/Components/WishListBooks/WishListBooks.jsx
+++ src/Components/WishListBooks/WishListBooks.jsx
@@ -20,16 +20,15 @@ const WishListBooks = () => {
                         <div>
                             <img className='h-48 mx-auto lg:w-40 rounded-xl ' src={book.image} alt={book.bookName} />
                         </div>
-                        <div className="flex flex-col items-center">
+                        <div className="flex flex-col items-center lg:items-start">
                             <h2 className='text-2xl font-semibold '>{book.bookName}</h2>
-                            <p className='text-lg mx-auto'>By : {book.author}</p>
-                            <div className="flex flex-col lg:flex-row items-center gap-5 text-lg my-3">
+                            <p className='text-lg '>By : {book.author}</p>
+                            <div className="flex flex-col lg:flex-row lg:items-center gap-5 text-lg my-3">
                                 <p className='font-bold flex gap-3'>Tags: {book.tags && book.tags.map((tag, index) => (
                                     <div key={index} className="badge text-base text-[#23BE0A] p-4 bg-[#ebf8e9]">#{tag}</div>
                                 ))}</p>
                                 <p className="flex items-center gap-1 "><CiLocationOn /> Year of Publishing: {book.yearOfPublishing}</p>
                             </div>
-                            <div></div>
                             <hr className="my-3 border border-l-neutral-900 w-full" />
                             <div className="flex gap-5">
                                 <button className="btn rounded-full  px-4 py-1 text-base bg-[#d1dbe7] text-[#328EFF]">Category: {book.category}</button>
